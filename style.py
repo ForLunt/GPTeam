903b50097b2af3c862d8721887cbb801b8d511bf
Morgane Pechon <97598276+ForLunt@users.noreply.github.com>
2023-04-02 05:14:58 +0200

    push style.py

--- style.py
+++ style.py
@@ -1,4 +1,5 @@
 import PIL
+import PIL as Image
 import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
@@ -7,44 +8,12 @@ from matplotlib import gridspec
 
 
 def load_image(image_path, image_size=(512, 256)):
+    base=tf.io.read_file(image_path)
     img = tf.io.decode_image(
-        tf.io.read_file(image_path),
+        base,
         channels=3, dtype=tf.float32)[tf.newaxis, ...]
-    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    #img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
     return img
-
-
-def visualize(images, titles=('',)):
-    noi = len(images)
-    image_sizes = [image.shape[1] for image in images]
-    w = (image_sizes[0] * 6) // 320
-    plt.figure(figsize=(w * noi, w))
-    grid_look = gridspec.GridSpec(1, noi, width_ratios=image_sizes)
-
-    for i in range(noi):
-        plt.subplot(grid_look[i])
-        plt.imshow(images[i][0], aspect='equal')
-        plt.axis('off')
-        plt.title(titles[i])
-        plt.savefig("final.jpg")
-    plt.show()
-
-
-original_image = load_image("input/mouette.jpeg")
-style_image = load_image("input/style.png")
-
-style_image = tf.nn.avg_pool(style_image, ksize=[3, 3], strides=[1, 1], padding='VALID')
-# visualize([original_image, style_image], ['Original Image', 'Style Image'])
-
-
-# A telecharger : https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2?tf-hub-format=compressed
-stylize_model = tf_hub.load("magenta")
-
-results = stylize_model(tf.constant(original_image), tf.constant(style_image))
-stylized_photo = results[0]
-
-results = stylize_model(tf.constant(original_image), tf.constant(style_image))
-stylized_photo = results[0]
 
 def export_image(tf_img):
     tf_img = tf_img * 255
@@ -54,5 +23,18 @@ def export_image(tf_img):
         img = tf_img[0]
     return PIL.Image.fromarray(img)
 
+def generate_img(original_img,style_img,nom):
+    # Brute force pour trouver des valeurs de ksize optimale 
+    style_img = tf.nn.avg_pool(style_img, ksize=[3, 3], strides=[1, 1], padding='VALID')
 
-export_image(stylized_photo).save("output/photo_stylise.png")
+    stylize_model = tf_hub.load("magenta")
+
+    results = stylize_model(tf.constant(original_img), tf.constant(style_img))
+    stylized_photo = results[0]
+    #stylized_photo = resize(original_img.size)
+    export_image(stylized_photo).save("output/"+nom+".png")
+
+generate_img(load_image("input/result.png"),load_image("input/comic.png"),"comicGen")
+generate_img(load_image("input/result.png"),load_image("input/van_gogh.png"),"van_gohgGen")
+generate_img(load_image("input/result.png"),load_image("input/stylized_ad.png"),"Stylized_adGen")
+
